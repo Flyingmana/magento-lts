a8b9ef401f112602bda4bdd3653e49539b0bb1dd
Flyingmana <flyingmana@googlemail.com>
2023-10-13 00:22:57 +0200

    debug

--- app/code/core/Mage/Core/Helper/String.php
+++ app/code/core/Mage/Core/Helper/String.php
@@ -88,6 +88,9 @@ class Mage_Core_Helper_String extends Mage_Core_Helper_Abstract
      */
     public function substr($string, $offset, $length = null)
     {
+        var_dump(__DIR__);
+        var_dump(__FILE__);
+        var_dump(getcwd());
         if (is_null($string)) {
             return '';
         }
